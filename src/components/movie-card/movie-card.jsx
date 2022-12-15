c78c5557358fe3123b2eed0e89331b75308f9209
brunahilbert <bruna.hilbertt@gmail.com>
2022-12-15 14:03:52 +0100

    implement prop-types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
       <div
@@ -8,4 +10,15 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         {movie.Title}
       </div>
     );
+  };
+
+  // Update in the future!!
+  MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      ImagePath: PropTypes.string,
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.string,
+      Genre: PropTypes.shape({Name: PropTypes.string})
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
   };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,38 +1,54 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, onBackClick }) => {
     return (
-      <div>
         <div>
-          <img src={movie.ImagePath} />
+            <div>
+                <img src={movie.ImagePath} />
+            </div>
+            <div>
+                <span>Title: </span>
+                <span>{movie.Title}</span>
+            </div>
+            <div>
+                <span>Release year: </span>
+                <span>{movie.releaseYear}</span>
+            </div>
+            <div>
+                <span>Duration: </span>
+                <span>{movie.Duration} minutes</span>
+            </div>
+            <div>
+                <span>Genre: </span>
+                <span>{movie.Genre.Name}</span>
+            </div>
+            <div>
+                <span>Description: </span>
+                <span>{movie.Description}</span>
+            </div>
+            <div>
+                <span>Actors: </span>
+                <span>{movie.Actors}</span>
+            </div>
+            <div>
+                <span>Director: </span>
+                <span>{movie.Director.Name}</span>
+            </div>
+            <button onClick={onBackClick}>Back</button>
         </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.Title}</span>
-        </div>
-        <div>
-          <span>Release year: </span>
-          <span>{movie.releaseYear}</span>
-        </div>
-        <div>
-          <span>Duration: </span>
-          <span>{movie.Duration} minutes</span>
-        </div>
-        <div>
-          <span>Genre: </span>
-          <span>{movie.Genre.Name}</span>
-        </div>
-        <div>
-          <span>Description: </span>
-          <span>{movie.Description}</span>
-        </div>
-        <div>
-          <span>Actors: </span>
-          <span>{movie.Actors}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movie.Director.Name}</span>
-        </div>
-        <button onClick={onBackClick}>Back</button>
-      </div>
     );
-  };
+};
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        ImagePath: PropTypes.string.isRequired,
+        Title: PropTypes.string.isRequired,
+        //   releaseYear: PropTypes.number.isRequired,
+        //   Duration: PropTypes.number.isRequired,
+        Genre: PropTypes.shape({ Name: PropTypes.string.isRequired }),
+        Description: PropTypes.string.isRequired,
+        //   Actors: PropTypes.array,
+        Director: PropTypes.shape({ Name: PropTypes.string.isRequired }),
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired
+};
