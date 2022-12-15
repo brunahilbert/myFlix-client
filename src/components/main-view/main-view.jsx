383ba8f92fd6fb3d05d48f7e95ea32b1dac38021
brunahilbert <bruna.hilbertt@gmail.com>
2022-12-15 14:03:06 +0100

    fetch api

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { MovieCard } from "../movie-card/movie-card";
 
@@ -6,6 +6,18 @@ import { MovieView } from "../movie-view/movie-view";
 
 // export = exposes the component, this makes it available for use by other components, modules, and files
 export const MainView = () => {
+    // the function assigned returns the visual representation of the component—in other words, the function renders what will be displayed on the screen.
+    const [movies, setMovies] = useState([]);
+
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://my-movie-box.herokuapp.com/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data);
+            });
+    },);
     }
   ]);
 
