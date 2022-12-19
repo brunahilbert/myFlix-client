64415164e1ef18f55366f79447940ab5577cd9df
brunahilbert <bruna.hilbertt@gmail.com>
2022-12-19 13:14:07 +0100

    implement login-view & signup-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,23 +1,44 @@
 import { useState, useEffect } from "react";
-
 import { MovieCard } from "../movie-card/movie-card";
-
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 // export = exposes the component, this makes it available for use by other components, modules, and files
 export const MainView = () => {
     // the function assigned returns the visual representation of the component—in other words, the function renders what will be displayed on the screen.
     const [movies, setMovies] = useState([]);
-
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const [user, setUser] = useState(storedUser ? storedUser : null);
+    const [token, setToken] = useState(storedToken ? storedToken : null);
 
     useEffect(() => {
-        fetch("https://my-movie-box.herokuapp.com/movies")
+        if (!token) {
+            return;
+        }
+        fetch("https://my-movie-box.herokuapp.com/movies", {
+            headers: { Authorization: `Bearer ${token}` }
+        })
             .then((response) => response.json())
             .then((data) => {
                 setMovies(data);
             });
-    });
+    }, [token]);
+
+    if (!user) {
+        return (
+            <>
+                <LoginView onLoggedIn={(user, token) => {
+                    setUser(user);
+                    setToken(token);
+                }} />
+                or
+                <SignupView />
+            </>
+        );
+    }
 
     if (selectedMovie) {
         let similarMovies = movies.filter(movie => movie.Genre.Name === selectedMovie.Genre.Name && movie._id !== selectedMovie._id);
