efa6f9a5454485a95295bf021881d5466ba372f8
brunahilbert <bruna.hilbertt@gmail.com>
2023-02-07 15:01:52 +0100

    customize movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,24 +1,37 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+
+import "./movie-card.scss";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.Title}
-      </div>
-    );
-  };
 
-  // Update in the future!!
-  MovieCard.propTypes = {
-    movie: PropTypes.shape({
-      ImagePath: PropTypes.string,
-      Title: PropTypes.string.isRequired,
-      Description: PropTypes.string,
-      Genre: PropTypes.shape({Name: PropTypes.string})
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
-  };
+  let description = movie.Description;
+  if (description.length > 100) {
+    description = description.substring(0, 100);
+  }
+
+  return (
+    <Card className="h-100 text-center">
+      <Card.Img className="p-3 pb-0" variant="top" src={movie.ImagePath} />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title>{`${movie.Title} (${movie.releaseYear})`}</Card.Title>
+        <Card.Text>{`${movie.Genre.Name} - ${movie.Duration} min`}</Card.Text>
+        <Card.Text>{description + "..."}</Card.Text>
+        <Button className="mt-auto" onClick={() => { onMovieClick(movie) }}>View</Button>
+      </Card.Body>
+    </Card>
+
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    ImagePath: PropTypes.string,
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string,
+    Genre: PropTypes.shape({ Name: PropTypes.string }),
+    releaseYear: PropTypes.number,
+    Duration: PropTypes.number
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
