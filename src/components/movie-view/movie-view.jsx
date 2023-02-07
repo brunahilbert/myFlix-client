9c68b1207b6e3f103571722665d1dfbd8a4bd4fe
brunahilbert <bruna.hilbertt@gmail.com>
2023-02-07 15:02:06 +0100

    customize movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,40 +1,42 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+
+import './movie-view.scss';
 
 export const MovieView = ({ movie, onBackClick }) => {
     return (
-        <div>
-            <div>
-                <img src={movie.ImagePath} />
+        <div className="card">
+
+            <img className="w-100 card-img-top" src={movie.ImagePath} />
+
+            <div className="card-body">
+                <h2 className="card-title text-center my-4">{movie.Title}</h2>
+                <div className="mb-3">
+                    <span className="fw-bold">Release year: </span>
+                    <span>{movie.releaseYear}</span><br />
+                </div>
+                <div className="mb-3">
+                    <span className="fw-bold">Duration: </span>
+                    <span>{movie.Duration} minutes</span><br />
+                </div>
+                <div className="mb-3">
+                    <span className="fw-bold">Genre: </span>
+                    <span>{movie.Genre.Name}</span><br />
+                </div>
+                <div className="mb-3">
+                    <span className="fw-bold">Description: </span>
+                    <span>{movie.Description}</span><br />
+                </div>
+                <div className="mb-3">
+                    <span className="fw-bold">Actors: </span>
+                    <span>{movie.Actors}</span><br />
+                </div>
+                <div className="mb-3">
+                    <span className="fw-bold">Director: </span>
+                    <span>{movie.Director.Name}</span><br />
+                </div>
             </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.Title}</span>
-            </div>
-            <div>
-                <span>Release year: </span>
-                <span>{movie.releaseYear}</span>
-            </div>
-            <div>
-                <span>Duration: </span>
-                <span>{movie.Duration} minutes</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.Genre.Name}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.Description}</span>
-            </div>
-            <div>
-                <span>Actors: </span>
-                <span>{movie.Actors}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.Director.Name}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
+            <Button onClick={onBackClick} className="mx-auto back-button">Back</Button>
         </div>
     );
 };
@@ -43,11 +45,11 @@ MovieView.propTypes = {
     movie: PropTypes.shape({
         ImagePath: PropTypes.string.isRequired,
         Title: PropTypes.string.isRequired,
-        //   releaseYear: PropTypes.number.isRequired,
-        //   Duration: PropTypes.number.isRequired,
+        releaseYear: PropTypes.number.isRequired,
+        Duration: PropTypes.number.isRequired,
         Genre: PropTypes.shape({ Name: PropTypes.string.isRequired }),
         Description: PropTypes.string.isRequired,
-        //   Actors: PropTypes.array,
+        Actors: PropTypes.array,
         Director: PropTypes.shape({ Name: PropTypes.string.isRequired }),
     }).isRequired,
     onBackClick: PropTypes.func.isRequired
