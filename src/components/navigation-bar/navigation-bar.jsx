f72c0b0065b663e7fa2d7e66c6be36741d2fb887
brunahilbert <bruna.hilbertt@gmail.com>
2023-02-20 11:46:26 +0100

    create navigation-bar.jsx

---
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -0,0 +1,43 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import "./navigation-bar.scss"
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+    return (
+        <Navbar className="fixed-top w-100" bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/" >
+                    <img src="https://cdn-icons-png.flaticon.com/512/1376/1376786.png" className="movie-box-logo me-2 d-inline-block align-top" alt="My movie box logo" />
+                    My Movie Box
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        {!user && (
+                            <>
+                                <Nav.Link as={Link} to="/login">
+                                    Login
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/signup">
+                                    Signup
+                                </Nav.Link>
+                            </>
+                        )}
+                        {user && (
+                            <>
+                                <Nav.Link as={Link} to="/">
+                                    Home
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/profile">
+                                    Profile
+                                </Nav.Link>
+                                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
