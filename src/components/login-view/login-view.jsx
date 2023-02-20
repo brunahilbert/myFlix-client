ba4d4fd5518dab4ab9149f2e88dce0d062234805
brunahilbert <bruna.hilbertt@gmail.com>
2023-02-20 11:43:43 +0100

    create routes to login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -11,32 +11,6 @@ export const LoginView = ({ onLoggedIn }) => {
         // this prevents the default behavior of the form which is to reload the entire page
         event.preventDefault();
 
-        //     const data = {
-        //       Username,
-        //       Password
-        //     };
-
-
-        // var myHeaders = new Headers();
-        // myHeaders.append("Content-Type", "application/json");
-
-        // var raw = JSON.stringify(data);
-
-        // fetch("https://my-movie-box.herokuapp.com/login", {
-        //     method: 'POST',
-        //     headers: myHeaders,
-        //     body: raw,
-        //     redirect: 'follow'
-        //   })
-        //   .then((response) => {
-        //     console.log(response.ok)
-        //           if (response.ok) {
-        //             onLoggedIn(Username);
-        //           } else {
-        //             alert("Login failed");
-        //           }
-        //         });
-
         const data = {
             Username: Username,
             Password: Password
@@ -61,6 +35,7 @@ export const LoginView = ({ onLoggedIn }) => {
                 }
             })
             .catch((e) => {
+                console.log(e)
                 alert("Something went wrong");
             });
 
